581a76e27ad20ac770a26053c37d43370efcb0ff
Ed Charbeneau <ed.charbeneau@gmail.com>
2020-06-16 12:14:22 -0400

    Add project files.

---
+++ BlazorDay/Services/LoggerService.cs
@@ -0,0 +1,25 @@
+﻿using GenFu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDay.Services
+{
+    public class LoggerService
+    {
+        
+        public LoggerService()
+        {
+
+            // Generate random data
+            var i = 0;
+            A.Configure<LogItem>()
+                .Fill(c => c.Id, () => { return i++; });
+
+            LogItems = A.ListOf<LogItem>();
+        }
+
+        public List<LogItem> LogItems { get; }
+    }
+}
