ed3513b9e722138e22d5dc715605efe051973fff
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-03-24 15:04:05 -0400

    Moved samples to .NET 5

---
+++ BlazorArtisan.Net5/Services/LogData.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BlazorArtisan
+{
+    public class LogItem
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public LogLevel Level { get; set; }
+    }
+}
---
+++ BlazorArtisan.Net5/Services/LoggerService.cs
@@ -0,0 +1,23 @@
+﻿using GenFu;
+using System.Collections.Generic;
+
+namespace BlazorArtisan.Services
+{
+    public class LoggerService
+    {
+        
+        public LoggerService()
+        {
+
+            // Generate random data
+            var i = 0;
+            A.Configure<LogItem>()
+                .Fill(li => li.Id, () => { return i++; })
+                .Fill(li => li.Message).AsLoremIpsumSentences(1);
+
+            LogItems = A.ListOf<LogItem>();
+        }
+
+        public List<LogItem> LogItems { get; }
+    }
+}
