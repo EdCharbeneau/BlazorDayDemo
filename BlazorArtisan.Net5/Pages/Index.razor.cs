fec3164f7948989e339925334e02e0fdc9687670
Ed Charbeneau <ed.charbeneau@gmail.com>
2021-06-10 12:42:06 -0400

    Fin

---
+++ BlazorArtisan.Net5/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlazorArtisan.Services;
+using Microsoft.Extensions.Logging;
+
+namespace BlazorArtisan.Net5.Pages
+{
+    
+    public partial class Index
+    {
+        //@inject
+        [Inject]
+        LoggerService logs { get; set; }
+        void DeleteItem(LogItem item)
+        {
+            logs.LogItems.Remove(item);
+        }
+
+       string CssMap(LogItem item) =>
+          item.Level switch
+          {
+              LogLevel.Warning => "alert-warning",
+              LogLevel.Critical => "alert-danger",
+              LogLevel.Error => "alert-danger",
+              _ => "alert-info"
+          };
+    }
+}
