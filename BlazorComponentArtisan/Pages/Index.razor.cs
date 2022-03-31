2d31ce89827216cdfdef50c7038883b64bcc091e
Ed Charbeneau <ed.charbeneau@gmail.com>
2022-03-31 16:18:30 -0400

    .NET7preview

---
+++ BlazorComponentArtisan/Pages/Index.razor.cs
@@ -0,0 +1,20 @@
+﻿using BlazorComponentArtisan.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorComponentArtisan.Pages;
+
+public partial class Index
+{
+    //@inject
+    [Inject]
+    LoggerService Logs { get; set; } = default!;
+    void DeleteItem(LogItem item) => Logs.LogItems.Remove(item);
+
+    string CssMap(LogItem item) => item.Level switch
+    {
+        LogLevel.Warning => "alert-warning",
+        LogLevel.Critical => "alert-danger",
+        LogLevel.Error => "alert-danger",
+        _ => "alert-info"
+    };
+}
---
+++ BlazorComponentArtisan/Services/LogData.cs
@@ -0,0 +1,6 @@
+﻿public class LogItem
+{
+    public int Id { get; set; }
+    public string Message { get; set; } = string.Empty;
+    public LogLevel Level { get; set; }
+}
